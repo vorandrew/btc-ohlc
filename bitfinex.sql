389c31c5368b9e3af4ea40a0eea8deb9ce681ecb
Andrey Vorobyov <vorandrew@gmail.com>
2015-10-17 21:56:31 +0300

    Improve window function

--- bitfinex.sql
+++ bitfinex.sql
@@ -3,25 +3,25 @@ DROP TABLE IF EXISTS time_bitfinex;
 DROP TABLE IF EXISTS ohlc_bitfinex;
 
 CREATE TABLE raw_bitfinex (
-  epoch INTEGER,
-  price DECIMAL(12,2),
-  volume DECIMAL(24,3)
+  epoch  INTEGER,
+  price  DECIMAL(12, 2),
+  volume DECIMAL(24, 3)
 );
 
 CREATE TABLE time_bitfinex (
-  dt TIMESTAMP WITHOUT TIME ZONE,
-  price DECIMAL(12,2),
-  volume DECIMAL(24,3)
+  dt     TIMESTAMP WITHOUT TIME ZONE,
+  price  DECIMAL(12, 2),
+  volume DECIMAL(24, 3)
 );
 
 CREATE INDEX ON time_bitfinex (dt);
 
 CREATE TABLE ohlc_bitfinex (
-  date TIMESTAMP WITHOUT TIME ZONE,
-  open DECIMAL(12,2),
-  high DECIMAL(12,2),
-  low DECIMAL(12,2),
-  close DECIMAL(12,2),
+  date   TIMESTAMP WITHOUT TIME ZONE,
+  open   DECIMAL(12, 2),
+  high   DECIMAL(12, 2),
+  low    DECIMAL(12, 2),
+  close  DECIMAL(12, 2),
   volume INTEGER
 );
 
@@ -34,43 +34,49 @@ COPY raw_bitfinex FROM '/tmp/bitfinexUSD.csv' DELIMITER ',' CSV;
 TRUNCATE time_bitfinex;
 
 INSERT INTO time_bitfinex
-SELECT
-  date_trunc('minute', to_timestamp(epoch)) "dt",
-  price,
-  volume
-FROM
-  raw_bitfinex
-ORDER BY dt;
+  SELECT
+    to_timestamp(epoch) "dt",
+    price,
+    volume
+  FROM
+    raw_bitfinex;
 
 TRUNCATE ohlc_bitfinex;
 
 INSERT INTO ohlc_bitfinex
-SELECT
-  DISTINCT
-  date_trunc('hour', dt) + INTERVAL '1 minute' * floor( EXTRACT( MINUTE from dt) / 30 ) * 30,
-  first_value(price) OVER w,
-  MAX(price) OVER w,
-  MIN(price) OVER w,
-  last_value(price) OVER w,
-  SUM(volume) OVER w
-FROM
-  (SELECT * FROM time_bitfinex ORDER BY dt) as t
-WINDOW w AS (
-  PARTITION BY
-    date_trunc('hour', dt) + INTERVAL '1 minute' * floor( EXTRACT( MINUTE from dt) / 30 ) * 30
-  ORDER BY
-    date_trunc('hour', dt) + INTERVAL '1 minute' * floor( EXTRACT( MINUTE from dt) / 30 ) * 30
-)
-ORDER BY date_trunc('hour', dt) + INTERVAL '1 minute' * floor( EXTRACT( MINUTE from dt) / 30 ) * 30;
+  WITH lines AS (SELECT
+                   date_trunc('hour', dt) + INTERVAL '1 minute' * floor(EXTRACT(MINUTE FROM dt) / 30) * 30 AS "date",
+                   first_value(price)
+                   OVER w                                                                                  AS "open",
+                   MAX(price)
+                   OVER w                                                                                  AS "high",
+                   MIN(price)
+                   OVER w                                                                                  AS "low",
+                   last_value(price)
+                   OVER w                                                                                  AS "close",
+                   SUM(volume)
+                   OVER w                                                                                  AS "volume"
+                 FROM
+                   time_bitfinex
+                 WINDOW
+                     w AS (
+                     PARTITION BY date_trunc('hour', dt) +
+                                  INTERVAL '1 minute' * floor(EXTRACT(MINUTE FROM dt) / 30) * 30
+                     ORDER BY dt
+                     ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING )
+  )
+  SELECT DISTINCT *
+  FROM lines
+  ORDER BY date;
 
 COPY (
-    SELECT
-        date "Date",
-        open "Open",
-        high "High",
-        low "Low",
-        close "Close",
-        volume "Volume"
-    FROM
-        ohlc_bitfinex
+SELECT
+  date   "Date",
+  open   "Open",
+  high   "High",
+  low    "Low",
+  close  "Close",
+  volume "Volume"
+FROM
+  ohlc_bitfinex
 ) TO '/tmp/BTCUSD.csv' WITH CSV;
--- bitstamp.sql
+++ bitstamp.sql
@@ -3,25 +3,25 @@ DROP TABLE IF EXISTS time_bitstamp;
 DROP TABLE IF EXISTS ohlc_bitstamp;
 
 CREATE TABLE raw_bitstamp (
-  epoch INTEGER,
-  price DECIMAL(12,2),
-  volume DECIMAL(24,3)
+  epoch  INTEGER,
+  price  DECIMAL(12, 2),
+  volume DECIMAL(24, 3)
 );
 
 CREATE TABLE time_bitstamp (
-  dt TIMESTAMP WITHOUT TIME ZONE,
-  price DECIMAL(12,2),
-  volume DECIMAL(24,3)
+  dt     TIMESTAMP WITHOUT TIME ZONE,
+  price  DECIMAL(12, 2),
+  volume DECIMAL(24, 3)
 );
 
 CREATE INDEX ON time_bitstamp (dt);
 
 CREATE TABLE ohlc_bitstamp (
-  date TIMESTAMP WITHOUT TIME ZONE,
-  open DECIMAL(12,2),
-  high DECIMAL(12,2),
-  low DECIMAL(12,2),
-  close DECIMAL(12,2),
+  date   TIMESTAMP WITHOUT TIME ZONE,
+  open   DECIMAL(12, 2),
+  high   DECIMAL(12, 2),
+  low    DECIMAL(12, 2),
+  close  DECIMAL(12, 2),
   volume INTEGER
 );
 
@@ -34,42 +34,49 @@ COPY raw_bitstamp FROM '/tmp/bitstampUSD.csv' DELIMITER ',' CSV;
 TRUNCATE time_bitstamp;
 
 INSERT INTO time_bitstamp
-SELECT
-  date_trunc('minute', to_timestamp(epoch)) "dt",
-  price,
-  volume
-FROM
-  raw_bitstamp;
+  SELECT
+    to_timestamp(epoch) "dt",
+    price,
+    volume
+  FROM
+    raw_bitstamp;
 
 TRUNCATE ohlc_bitstamp;
 
 INSERT INTO ohlc_bitstamp
-SELECT
-  DISTINCT
-  date_trunc('hour', dt) + INTERVAL '1 minute' * floor( EXTRACT( MINUTE from dt) / 30 ) * 30,
-  first_value(price) OVER w,
-  MAX(price) OVER w,
-  MIN(price) OVER w,
-  last_value(price) OVER w,
-  SUM(volume) OVER w
-FROM
-  (SELECT * FROM time_bitstamp ORDER BY dt) as t
-WINDOW w AS (
-  PARTITION BY
-    date_trunc('hour', dt) + INTERVAL '1 minute' * floor( EXTRACT( MINUTE from dt) / 30 ) * 30
-  ORDER BY
-    date_trunc('hour', dt) + INTERVAL '1 minute' * floor( EXTRACT( MINUTE from dt) / 30 ) * 30
-)
-ORDER BY date_trunc('hour', dt) + INTERVAL '1 minute' * floor( EXTRACT( MINUTE from dt) / 30 ) * 30;
+  WITH lines AS (SELECT
+                   date_trunc('hour', dt) + INTERVAL '1 minute' * floor(EXTRACT(MINUTE FROM dt) / 30) * 30 AS "date",
+                   first_value(price)
+                   OVER w                                                                                  AS "open",
+                   MAX(price)
+                   OVER w                                                                                  AS "high",
+                   MIN(price)
+                   OVER w                                                                                  AS "low",
+                   last_value(price)
+                   OVER w                                                                                  AS "close",
+                   SUM(volume)
+                   OVER w                                                                                  AS "volume"
+                 FROM
+                   time_bitstamp
+                 WINDOW
+                     w AS (
+                     PARTITION BY date_trunc('hour', dt) +
+                                  INTERVAL '1 minute' * floor(EXTRACT(MINUTE FROM dt) / 30) * 30
+                     ORDER BY dt
+                     ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING )
+  )
+  SELECT DISTINCT *
+  FROM lines
+  ORDER BY date;
 
 COPY (
-    SELECT
-        date "Date",
-        open "Open",
-        high "High",
-        low "Low",
-        close "Close",
-        volume "Volume"
-    FROM
-        ohlc_bitstamp
+SELECT
+  date   "Date",
+  open   "Open",
+  high   "High",
+  low    "Low",
+  close  "Close",
+  volume "Volume"
+FROM
+  ohlc_bitstamp
 ) TO '/tmp/BTCUSD.csv' WITH CSV;
